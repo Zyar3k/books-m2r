574679d59da4725f028443a790060c4ab0fb5232
Arkadiusz Zygan <a.zygan@gmail.com>
2021-03-02 20:48:14 +0100

    Add routes, controllers & model

--- server/server.js
+++ server/server.js
@@ -4,11 +4,14 @@ const cors = require("cors");
 
 const config = require("./config");
 
+const bookRoutes = require("./routes/book.routes");
+
 const app = express();
 
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use("/", bookRoutes);
 
 mongoose.connect(config.DB, {
   useNewUrlParser: true,
