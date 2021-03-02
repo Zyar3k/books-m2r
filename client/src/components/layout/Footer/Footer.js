517add6ebeab24953f92b3b6ca176df6ebbab53b
Arkadiusz Zygan <a.zygan@gmail.com>
2021-03-03 00:33:11 +0100

    UI/UX in progress

--- client/src/components/layout/Footer/Footer.js
+++ client/src/components/layout/Footer/Footer.js
@@ -1,10 +1,35 @@
+import { useState } from "react";
+
 import styles from "./Footer.module.scss";
 
 const Footer = () => {
+  const [isFilter, setIsFilter] = useState(false);
+
+  const filterToggle = () => setIsFilter(!isFilter);
   return (
     <footer>
+      {isFilter ? (
+        <div className={styles.filterIsOn}>
+          <div className={styles.filtrWrapper}>
+            <i className='fas fa-chevron-up'></i>
+            <p>Sortuj po nazwisku</p>
+            <i className='fas fa-chevron-down'></i>
+          </div>
+          <div className={styles.filtrWrapper}>
+            <i className='fas fa-chevron-up'></i>
+            <p>Sortuj po imieniu</p>
+            <i className='fas fa-chevron-down'></i>
+          </div>
+          <div className={styles.filtrWrapper}>
+            <i className='fas fa-chevron-up'></i>
+            <p>Sortuj po ilości stron</p>
+            <i className='fas fa-chevron-down'></i>
+          </div>
+        </div>
+      ) : null}
       <div className={styles.footer}>
         <p>@Zyar3k 2021</p>
+        <p onClick={filterToggle}>Filtruj</p>
         <div className={styles.contactShort}>
           <p>Masz uwagi? Napisz:</p>
           <i className='fas fa-envelope'></i>
