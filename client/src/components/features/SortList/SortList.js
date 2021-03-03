e3f0e328f87e97241685b1bc444dac7918d68c39
Arkadiusz Zygan <a.zygan@gmail.com>
2021-03-03 13:16:56 +0100

    Change default radioinput view

--- client/src/components/features/SortList/SortList.js
+++ client/src/components/features/SortList/SortList.js
@@ -4,39 +4,75 @@ const SortList = ({ closeMobileMenu }) => {
   return (
     <>
       <li onClick={closeMobileMenu} className={styles.navItem}>
-        <label htmlFor='all'>
-          <p>m2r</p>
-          <input type='radio' name='listSelect' id='all' defaultChecked />
+        <label
+          htmlFor='all'
+          className={`${styles.radio} ${styles.radioBefore}`}
+        >
+          <span className={styles.radioInput}>
+            <input type='radio' name='listSelect' id='all' defaultChecked />
+            <span className={styles.radioControl}></span>
+          </span>
+          <span className={styles.radioLabel}>B2R</span>
         </label>
       </li>
       <li onClick={closeMobileMenu} className={styles.navItem}>
-        <label htmlFor='ama'>
-          <p>amazon</p>
-          <input type='radio' name='listSelect' id='ama' />
+        <label
+          htmlFor='ama'
+          className={`${styles.radio} ${styles.radioBefore}`}
+        >
+          <span className={styles.radioInput}>
+            <input type='radio' name='listSelect' id='ama' />
+            <span className={styles.radioControl}></span>
+          </span>
+          <span className={styles.radioLabel}>Amazon</span>
         </label>
       </li>
       <li onClick={closeMobileMenu} className={styles.navItem}>
-        <label htmlFor='bbc'>
-          <p>bbc</p>
-          <input type='radio' name='listSelect' id='bbc' />
+        <label
+          htmlFor='bbc'
+          className={`${styles.radio} ${styles.radioBefore}`}
+        >
+          <span className={styles.radioInput}>
+            <input type='radio' name='listSelect' id='bbc' />
+            <span className={styles.radioControl}></span>
+          </span>
+          <span className={styles.radioLabel}>BBC</span>
         </label>
       </li>
       <li onClick={closeMobileMenu} className={styles.navItem}>
-        <label htmlFor='emp'>
-          <p>empik</p>
-          <input type='radio' name='listSelect' id='emp' />
+        <label
+          htmlFor='emp'
+          className={`${styles.radio} ${styles.radioBefore}`}
+        >
+          <span className={styles.radioInput}>
+            <input type='radio' name='listSelect' id='emp' />
+            <span className={styles.radioControl}></span>
+          </span>
+          <span className={styles.radioLabel}>Empik</span>
         </label>
       </li>
       <li onClick={closeMobileMenu} className={styles.navItem}>
-        <label htmlFor='gan'>
-          <p>gandalf</p>
-          <input type='radio' name='listSelect' id='gan' />
+        <label
+          htmlFor='gan'
+          className={`${styles.radio} ${styles.radioBefore}`}
+        >
+          <span className={styles.radioInput}>
+            <input type='radio' name='listSelect' id='gan' />
+            <span className={styles.radioControl}></span>
+          </span>
+          <span className={styles.radioLabel}>Gandalf</span>
         </label>
       </li>
       <li onClick={closeMobileMenu} className={styles.navItem}>
-        <label htmlFor='pozy'>
-          <p>pozycje</p>
-          <input type='radio' name='listSelect' id='pozy' />
+        <label
+          htmlFor='pozy'
+          className={`${styles.radio} ${styles.radioBefore}`}
+        >
+          <span className={styles.radioInput}>
+            <input type='radio' name='listSelect' id='pozy' />
+            <span className={styles.radioControl}></span>
+          </span>
+          <span className={styles.radioLabel}>Pozycje</span>
         </label>
       </li>
     </>
