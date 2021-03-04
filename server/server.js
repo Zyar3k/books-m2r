fc4f0c8d26a2eff0a88462aa733fbac72a3fc9fe
Arkadiusz Zygan <a.zygan@gmail.com>
2021-03-04 22:42:08 +0100

    Add dotenv to project

--- server/server.js
+++ server/server.js
@@ -1,19 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-
-const config = require("./config");
+const dotenv = require("dotenv");
 
 const bookRoutes = require("./routes/book.routes");
 
 const app = express();
+dotenv.config();
 
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use("/", bookRoutes);
 
-mongoose.connect(config.DB, {
+mongoose.connect(process.env.DB, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -23,7 +23,7 @@ let db = mongoose.connection;
 db.once("open", () => console.log("Wow!! You're connected to the database!"));
 db.on("error", (error) => console.log("Error " + error));
 
-app.listen(process.env.PORT || config.PORT, () => {
-  console.log(`Server is running on port: ${config.PORT}`);
+app.listen(process.env.PORT || 8000, () => {
+  console.log(`Server is running on port: 8000`);
   console.log(`Happy coding`);
 });
