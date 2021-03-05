0f94686e6b5cbed0b77ba85041056fa97923c1b1
Arkadiusz Zygan <a.zygan@gmail.com>
2021-03-05 17:44:11 +0100

    Add statscomp to mobile view

--- client/src/components/layout/Footer/Footer.js
+++ client/src/components/layout/Footer/Footer.js
@@ -1,17 +1,29 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+
+import { StoreContext } from "../../../store/StoreProvider";
+
 import FilterModule from "../../features/FilterModule/FilterModule";
 import SortList from "../../features/SortList/SortList";
+import StatsModule from "../../features/StatsModule/StatsModule";
 
 import styles from "./Footer.module.scss";
 
 const Footer = () => {
+  const { isExtension } = useContext(StoreContext);
   const [isSorted, setIsSorted] = useState(false);
   const [isFiltered, setIsFiltered] = useState(false);
+  const [isStats, setIsStats] = useState(false);
 
   const sortToggle = () => setIsSorted(!isSorted);
   const filterToggle = () => setIsFiltered(!isFiltered);
+  const statsToggle = () => setIsStats(!isStats);
   return (
     <footer>
+      {isStats ? (
+        <div className={styles.filterIsOn}>
+          <StatsModule />
+        </div>
+      ) : null}
       {isSorted ? (
         <div className={styles.filterIsOn}>
           <SortList />
@@ -27,20 +39,32 @@ const Footer = () => {
           @Zyar3k
           <span>2021</span>
         </p>
-        <button
-          disabled={isFiltered ? true : false}
-          className={styles.filterToggle}
-          onClick={sortToggle}
-        >
-          Sortuj
-        </button>
-        <button
-          disabled={isSorted ? true : false}
-          className={styles.filterToggle}
-          onClick={filterToggle}
-        >
-          Filtruj
-        </button>
+        {isExtension ? (
+          <button
+            disabled={isFiltered ? true : false}
+            className={styles.filterToggle}
+            onClick={statsToggle}
+          >
+            Statystyki
+          </button>
+        ) : (
+          <>
+            <button
+              disabled={isFiltered ? true : false}
+              className={styles.filterToggle}
+              onClick={sortToggle}
+            >
+              Sortuj
+            </button>
+            <button
+              disabled={isSorted ? true : false}
+              className={styles.filterToggle}
+              onClick={filterToggle}
+            >
+              Filtruj
+            </button>
+          </>
+        )}
         <div className={styles.contactShort}>
           <p>
             <span>Masz uwagi?</span>
