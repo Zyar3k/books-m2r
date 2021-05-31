71cefe7850e1b3ab59231939fd8d2eb2c636eaba
Arkadiusz Zygan <a.zygan@gmail.com>
2021-05-31 20:51:52 +0200

    make it better

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const DB_MONGO = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS}@cluster0-y9wgt.mongodb.net/DBBooks`;
+const PORT = process.env.PORT || 8000;
 
 const bookRoutes = require("./routes/book.routes");
 
@@ -17,7 +19,7 @@ app.get("/", (req, res) => {
   res.send("Hello, m2r API");
 });
 
-mongoose.connect(process.env.DB, {
+mongoose.connect(DB_MONGO, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -27,7 +29,7 @@ let db = mongoose.connection;
 db.once("open", () => console.log("Wow!! You're connected to the database!"));
 db.on("error", (error) => console.log("Error " + error));
 
-app.listen(process.env.PORT || 8000, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on port: 8000`);
   console.log(`Happy coding`);
 });
